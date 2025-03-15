9d50f47c2405e728bba6a841b28437b8d0852eca
=TiJ-code <78140568+TiJ-code@users.noreply.github.com>
2025-03-15 21:58:09 +0100

    Restructure

--- src/main/java/dk/tij/registermaschine/RegistermaschineLauncher.java
+++
@@ -1,7 +0,0 @@
-package dk.tij.registermaschine;
-
-public class RegistermaschineLauncher {
-    public static void main(String[] args) {
-        RegisterApplication.main(args);
-    }
-}
--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module registermaschine {
-    requires javafx.web;
-    requires jdk.jsobject;
-    requires org.fxmisc.richtext;
-
-    exports dk.tij.registermaschine;
-    exports dk.tij.registermaschine.parser;
-    exports dk.tij.registermaschine.editor;
-}
