79c7bd7a439e7babe66e414c90c29e29d925f7b0
TιJ <78140568+TiJ-code@users.noreply.github.com>
2026-03-10 21:13:37 +0100

    [CORE-007] Documentation of Errors
    (#37)

--- registermaschine-api/src/main/java/dk/tij/registermaschine/api/error/ClassInstantiationException.java
+++ registermaschine-api/src/main/java/dk/tij/registermaschine/api/error/ClassInstantiationException.java
@@ -1,5 +1,14 @@
 package dk.tij.registermaschine.api.error;
 
+/**
+ * Thrown when a class cannot be instantiated via reflection or other
+ * dynamic mechanisms.
+ *
+ * <p>This exception wraps the underlying exception that caused the failure.</p>
+ *
+ * @since 1.0.0
+ * @author TiJ
+ */
 public class ClassInstantiationException extends RuntimeException {
     public ClassInstantiationException(String message, Exception e) {
         super(message, e);
--- registermaschine-api/src/main/java/dk/tij/registermaschine/api/error/UnknownInstructionException.java
+++ registermaschine-api/src/main/java/dk/tij/registermaschine/api/error/UnknownInstructionException.java
@@ -1,5 +1,12 @@
 package dk.tij.registermaschine.api.error;
 
+/**
+ * Thrown when an instruction cannot be identified or is not registered
+ * in the current instruction set.
+ *
+ * @since 1.0.0
+ * @author TiJ
+ */
 public class UnknownInstructionException extends RuntimeException {
     public UnknownInstructionException(String message) {
         super(message);
