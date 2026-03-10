176466b02abd6389b1f04eabc1d03e522a77e338
TιJ <78140568+TiJ-code@users.noreply.github.com>
2026-03-10 21:13:37 +0100

    [CORE-007] Documentation of Errors
    (#37)

--- registermaschine-api/src/main/java/dk/tij/registermaschine/api/error/DefectPipelineException.java
+++ registermaschine-api/src/main/java/dk/tij/registermaschine/api/error/DefectPipelineException.java
@@ -1,5 +1,15 @@
 package dk.tij.registermaschine.api.error;
 
+/**
+ * Thrown when an instruction pipeline is broken or cannot be executed
+ * properly due to internal errors.
+ *
+ * <p>This exception wraps the underlying exception that caused the pipeline
+ * failure.</p>
+ *
+ * @since 1.0.0
+ * @author TiJ
+ */
 public class DefectPipelineException extends RuntimeException {
     public DefectPipelineException(String msg, Exception e) {
         super(msg, e);
