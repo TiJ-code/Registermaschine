77c7d87e81a350b793339b22665f0f7292be24c9
TιJ <78140568+TiJ-code@users.noreply.github.com>
2026-03-10 21:13:37 +0100

    [CORE-007] Documentation of Errors
    (#37)

--- registermaschine-api/src/main/java/dk/tij/registermaschine/api/error/SyntaxErrorException.java
+++ registermaschine-api/src/main/java/dk/tij/registermaschine/api/error/SyntaxErrorException.java
@@ -1,5 +1,11 @@
 package dk.tij.registermaschine.api.error;
 
+/**
+ * Thrown when there is a syntax error in the source code.
+ *
+ * @since 1.0.0
+ * @author TiJ
+ */
 public class SyntaxErrorException extends RuntimeException {
     public SyntaxErrorException(String message) {
         super(message);
