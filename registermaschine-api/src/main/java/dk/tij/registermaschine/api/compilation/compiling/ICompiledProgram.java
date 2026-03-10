ed04a9efc0429fbf1a4654e3335020cf8bd51dec
TιJ <78140568+TiJ-code@users.noreply.github.com>
2026-03-10 23:13:27 +0100

    [CORE-007] Documentation of compilation
    (#37)

--- registermaschine-api/src/main/java/dk/tij/registermaschine/api/compilation/compiling/ICompiledProgram.java
+++ registermaschine-api/src/main/java/dk/tij/registermaschine/api/compilation/compiling/ICompiledProgram.java
@@ -2,5 +2,15 @@ package dk.tij.registermaschine.api.compilation.compiling;
 
 import java.util.List;
 
+/**
+ * Represents a compiled program consisting of a sequence of
+ * {@link ICompiledInstruction compiled instructions}.
+ *
+ * <p>This interface extends both {@link Iterable} and {@link List}
+ * for convenient iteration and random access.</p>
+ *
+ * @since 1.0.0
+ * @author TiJ
+ */
 public interface ICompiledProgram extends Iterable<ICompiledInstruction>, List<ICompiledInstruction> {
 }
