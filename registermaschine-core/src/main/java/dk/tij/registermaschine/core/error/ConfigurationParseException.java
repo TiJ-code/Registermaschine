28098bcd7ef19dc97bbfddf34890da6c74b47251
TιJ <78140568+TiJ-code@users.noreply.github.com>
2026-03-10 21:13:37 +0100

    [CORE-007] Documentation of Errors
    (#37)

--- registermaschine-core/src/main/java/dk/tij/registermaschine/core/error/ConfigurationParseException.java
+++ registermaschine-core/src/main/java/dk/tij/registermaschine/core/error/ConfigurationParseException.java
@@ -1,5 +1,13 @@
 package dk.tij.registermaschine.core.error;
 
+/**
+ * Thrown when a configuration file or object cannot be parsed correctly.
+ *
+ * <p>This exception may wrap another exception that triggered the parse failure.</p>
+ *
+ * @since 1.0.0
+ * @author TiJ
+ */
 public class ConfigurationParseException extends RuntimeException {
     public ConfigurationParseException(String message, Exception e) {
         super(message, e);
